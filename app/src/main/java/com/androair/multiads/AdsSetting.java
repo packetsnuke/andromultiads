7c4230b9ba1bbf864796d82bbf5181cc5e251229
packetsnuke <packetsnuke@gmail.com>
2021-08-21 11:43:45 +0700

    Add Ads Setting, update gradle

---
+++ app/src/main/java/com/androair/multiads/AdsSetting.java
@@ -0,0 +1,26 @@
+package com.androair.multiads;
+
+public class AdsSetting {
+
+    public static String SELECT_ADS ="STARTAPP";
+    public static String BACKUP_ADS = "APPLOVIN-D";
+
+    public static String MAIN_ADS_BANNER ="DefaultBanner";
+    public static String BACKUP_ADS_BANNER="db4d5e8718b97d78";
+
+    public static String MAIN_ADS_INTERTITIAL ="DefaultInterstitial";
+    public static String BACKUP_ADS_INTERTITIAL="db4d5e8718b97d78";
+
+    public static String MAIN_ADS_REWARDS ="DefaultRewardedVideo";
+    public static String BACKUP_ADS_REWARDS="db4d5e8718b97d78";
+
+    public static String OPEN_ADS_ADMOB ="ca-app-pub-3940256099942544/3419835294";
+    public static String NATIVE_ADS_ADMOB ="ca-app-pub-3940256099942544/2247696110";
+    public static String INITIALIZE_SDK = "107355779";
+
+    public static String HPK1="";
+    public static String HPK2="";
+    public static String HPK3="";
+    public static String HPK4="";
+    public static String HPK5="";
+}
